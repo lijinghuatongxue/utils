56b26b23055d815239322495a34fae995fadaa19
lijinghuatongxue <woshilijinghua@gmail.com>
2021-03-27 18:29:41 +0800

    🚩： 增加 Get IP or domain name Ping delay，IPv4 only

---
+++ GetIpv4IPICMP.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/tatsushid/go-fastping"
+	"net"
+	"os"
+	"time"
+)
+
+func GetIpv4IpICMP(Ipv4Addr string) {
+	p := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr("ip4:icmp", Ipv4Addr)
+	if err != nil {
+		logrus.Error(err)
+		os.Exit(1)
+	}
+	p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		logrus.Infof("IP Addr: %s receive, 往返时延: %v\n", addr.String(), rtt)
+	}
+	//p.OnIdle = func() {
+	//	fmt.Println("finish")
+	//}
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// 获取IP或者域名的ping延迟，仅限于Ipv4
+// Get IP or domain name Ping delay，IPv4 only
+func main() {
+	DomainName := "google.com"
+	//DomainName := "8.8.8.8"
+	GetIpv4IpICMP(DomainName)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/go-git/go-git/v5 v5.2.0
 	github.com/sirupsen/logrus v1.7.0
+	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/text v0.3.5 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
