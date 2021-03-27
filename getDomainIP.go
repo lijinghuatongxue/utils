fb14cae2bcfe46bbfeed6d60752c265eab8f3160
lijinghuatongxue <woshilijinghua@gmail.com>
2021-03-27 18:11:06 +0800

    🚩： 增加 Get IP address of domain name

---
+++ getDomainIP.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"net"
+	"time"
+)
+
+func GetDomainIP(DNSServer, Domain string) {
+	r := &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			d := net.Dialer{
+				Timeout: time.Millisecond * time.Duration(10000),
+			}
+			return d.DialContext(ctx, network, DNSServer+":53")
+		},
+	}
+	ip, _ := r.LookupHost(context.Background(), Domain)
+	if len(ip) == 0 {
+		logrus.Errorf("Domain Resolver Err! -> %s |DNSServer -> %s |IP -> %s", Domain, DNSServer, ip)
+	} else {
+		logrus.Infof("Domain -> %s |DNSServer -> %s |IP -> %s", Domain, DNSServer, ip[0])
+	}
+
+}
+
+// 获取域名的IP
+// Get IP address of domain name
+func main() {
+	GetDomainIP("8.8.8.8", "lijinghua.club")
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module util/v1
 go 1.15
 
 require (
-	github.com/go-git/go-billy/v5 v5.0.0
 	github.com/go-git/go-git/v5 v5.2.0
 	github.com/sirupsen/logrus v1.7.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
