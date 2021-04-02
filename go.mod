8e49273b2ef0efb355003a4b420b241a2942bba0
woshilijinghua@gmail.com <woshilijinghua@gmail.com>
2021-04-02 20:36:47 +0800

    🚩： add progressBar

--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module util/v1
 go 1.15
 
 require (
+	github.com/cheggaaa/pb/v3 v3.0.7
 	github.com/go-git/go-git/v5 v5.2.0
-	github.com/reiver/go-oi v1.0.0 // indirect
-	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e
 	github.com/sirupsen/logrus v1.7.0
 	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
---
+++ progressBar.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
+)
+
+type ProgressBarStruct struct {
+	FuncName string
+	LoopNum  int64
+}
+
+func ProgressBar(FuncName string, Count int64) {
+	count := Count
+	// create and start new bar
+	//bar := pb.StartNew(count)
+
+	// start bar from 'default' template
+	// bar := pb.Default.Start(count)
+
+	// start bar from 'simple' template
+	// bar := pb.Simple.Start(count)
+
+	// start bar from 'full' template
+	// bar := pb.Full.Start(count)
+
+	tmpl := fmt.Sprintf("{{ red \"%s:\" }} {{ bar . \"[-\" \"-\" (cycle . \"—>\" \"↖\" \"↗\" \"↘\" \"↙\") \".\" \">]\"}} {{speed . | rndcolor }} {{percent .}}  {{string . \"my_green_string\" | green}} {{string . \"my_blue_string\" | blue}}", FuncName)
+	// start bar based on our template
+	bar := pb.ProgressBarTemplate(tmpl).Start64(count)
+	// set values for string elements
+	//bar.Set("my_green_string", "描述1").Set("my_blue_string", "描述2")
+	for i := 0; i < int(count); i++ {
+		bar.Increment()
+		Task()
+	}
+	bar.Finish()
+}
+
+func Task() {
+	time.Sleep(1 * time.Second)
+	// 尽量不要有输出，否则会换行
+	// Try not to have output, otherwise I will wrap.
+	// Try not to have output, otherwise I will wrap.
+	fmt.Println("ProgressBar test ..")
+}
+
+//需要指定进度条前缀、处理的次数，比如处理多少个文件，发出多少个请求之类的
+//You need to specify the progress bar prefix, the number of times processed, such as how many files are processed, how many requests are made, and so on.
+//总等待时间 = 次数 * 你程序消耗时间
+//Total waiting time = times * your program consumes time
+func main() {
+	ProgressBar("test", 10)
+}
