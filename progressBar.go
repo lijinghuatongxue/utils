38774df75b18cfa6df75631bcca26ed2c55cdc42
lijinghuatongxue <woshilijinghua@gmail.com>
2021-04-03 09:40:11 +0800

    fea: add progressBar

--- progressBar.go
+++ progressBar.go
@@ -3,10 +3,9 @@ package meUtils
 import (
 	"fmt"
 	"github.com/cheggaaa/pb/v3"
-	"time"
 )
 
-func ProgressBar(FuncName string, Count int64) {
+func ProgressBar(FuncName string, Count int64, Func func()) {
 	count := Count
 	// create and start new bar
 	//bar := pb.StartNew(count)
@@ -27,18 +26,18 @@ func ProgressBar(FuncName string, Count int64) {
 	//bar.Set("my_green_string", "描述1").Set("my_blue_string", "描述2")
 	for i := 0; i < int(count); i++ {
 		bar.Increment()
-		ProgressBarTask()
+		Func()
 	}
 	bar.Finish()
 }
 
-func ProgressBarTask() {
-	time.Sleep(1 * time.Second)
-	// 尽量不要有输出，否则会换行
-	// Try not to have output, otherwise I will wrap.
-	// Try not to have output, otherwise I will wrap.
-	fmt.Println("ProgressBar test ..")
-}
+//func ProgressBarTask() {
+//	time.Sleep(1 * time.Second)
+//	// 尽量不要有输出，否则会换行
+//	// Try not to have output, otherwise I will wrap.
+//	// Try not to have output, otherwise I will wrap.
+//	fmt.Println("ProgressBar test ..")
+//}
 
 //需要指定进度条前缀、处理的次数，比如处理多少个文件，发出多少个请求之类的
 //You need to specify the progress bar prefix, the number of times processed, such as how many files are processed, how many requests are made, and so on.
