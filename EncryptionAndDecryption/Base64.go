ca66f404aacd6eb1639554621500850cfc284415
lijinghuatongxue <woshilijinghua@gmail.com>
2021-04-10 23:44:43 +0800

    fea: add base64 encode and decode

---
+++ EncryptionAndDecryption/Base64.go
@@ -0,0 +1,11 @@
+package EncryptionAndDecryption
+
+import "encoding/base64"
+
+func Base64Encode(src []byte) []byte {
+	return []byte(base64.StdEncoding.EncodeToString(src))
+}
+
+func Base64Decode(src []byte) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(string(src))
+}
---
+++ cryptography.go
@@ -0,0 +1,28 @@
+package meUtils
+
+import (
+	"github.com/lijinghuatongxue/utils/EncryptionAndDecryption"
+	"github.com/sirupsen/logrus"
+)
+
+func Base64Encode(encodeStr string) string {
+	// encode
+	deByte := EncryptionAndDecryption.Base64Encode([]byte(encodeStr))
+	return string(deByte)
+}
+
+func Base64Decode(decodeStr []byte) string {
+	// decode
+	enByte, err := EncryptionAndDecryption.Base64Decode(decodeStr)
+	if err != nil {
+		logrus.Error(err.Error())
+	}
+	return string(enByte)
+}
+
+//func main()  {
+//	i := Base64Encode("233")
+//	logrus.Infof("enCode str is %s",i)
+//	j := Base64Decode([]byte("MjMz"))
+//	logrus.Infof("deCode str is %s",j)
+//}
