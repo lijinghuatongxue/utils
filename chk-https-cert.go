55732dd7ec85f8436bec3602ab8e7d8f75c254df
lijinghuatongxue <woshilijinghua@gmail.com>
2021-06-04 14:25:54 +0800

    fea: HTTPS证书有效期判断/ Judgment of validity period of HTTPS certificate

--- chk-https-cert.go
+++ chk-https-cert.go
@@ -8,17 +8,18 @@ import (
 )
 
 //获取相差时间
-func getHourDiffer(startTime, endTime string) int64 {
+func getHourDiffer(startTime, endTime string, DetailedOutput bool) int64 {
 	a, _ := time.Parse("2006-01-02 15:04:05", startTime)
 	b, _ := time.Parse("2006-01-02 15:04:05", endTime)
 	d := b.Sub(a)
-	logrus.Infof("距离域名过期还有 -> %d天 ｜%d小时 ｜%d分钟", int64(d.Hours()/24), int64(d.Hours()), int64(d.Minutes()))
+	if DetailedOutput {
+		logrus.Infof("距离域名过期还有 -> %d天 ｜%d小时 ｜%d分钟", int64(d.Hours()/24), int64(d.Hours()), int64(d.Minutes()))
+	}
 	return int64(d.Hours() / 24)
 }
 
 // 检测https域名的证书有效期
-func ChkHTTPSCert(domainName string) {
-	domainName = domainName
+func ChkHTTPSCert(domainName string, DetailedOutput bool) int64 {
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
@@ -33,10 +34,17 @@ func ChkHTTPSCert(domainName string) {
 
 	//fmt.Println(resp.TLS.PeerCertificates[0])
 	certInfo := resp.TLS.PeerCertificates[0]
-	logrus.Info("过期时间:", certInfo.NotAfter)
-	logrus.Info("组织信息:", certInfo.Subject)
+	if DetailedOutput {
+		logrus.Info("过期时间:", certInfo.NotAfter)
+		logrus.Info("组织信息:", certInfo.Subject)
+	}
 
 	nowTime := time.Now().Format("2006-01-02 15:04:05")
 	endTime := certInfo.NotAfter.Format("2006-01-02 15:04:05")
-	getHourDiffer(nowTime, endTime)
+	return getHourDiffer(nowTime, endTime, DetailedOutput)
 }
+
+//func main()  {
+//	time := ChkHTTPSCert("https://lijinghua.club",false)
+//	logrus.Warn(time)
+//}
