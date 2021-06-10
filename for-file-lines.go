7cc133c1ef8b1ee867133866d801909da1a86173
lijinghuatongxue <woshilijinghua@gmail.com>
2021-06-10 19:55:40 +0800

    fea: add Read each line of the file in a row cycle And Perform simple functions

--- for-file-lines.go
+++ for-file-lines.go
@@ -9,7 +9,7 @@ import (
 )
 
 // 按行读取文件， 传入文件路径
-func ForFileLines(FileName string) ([]string, error) {
+func ForFileLines(FileName string, DetailedOutput bool) ([]string, error) {
 	fi, err := os.Open(FileName)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
@@ -26,7 +26,9 @@ func ForFileLines(FileName string) ([]string, error) {
 		}
 
 		LineArrStr = append(LineArrStr, string(a))
-		logrus.Info(string(a))
+		if DetailedOutput {
+			logrus.Info(string(a))
+		}
 	}
 	return LineArrStr, nil
 }
