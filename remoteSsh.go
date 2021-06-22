77ef8232e6395005029d2700d4dc9b5a119828df
lijinghuatongxue <woshilijinghua@gmail.com>
2021-06-22 12:26:15 +0800

    fea: add remote ssh fea

--- remoteSsh.go
+++ remoteSsh.go
@@ -16,18 +16,18 @@ func RemoteCmd(IP, Port, User, CMD, idRsaPath string) (string, bool) {
 	port := Port
 	key, err := ioutil.ReadFile(idRsaPath)
 	if err != nil {
-		logrus.Error("[util - remote-ssh] | ❌ false|unable to read private key: %v", err)
+		logrus.Errorf("[util - remote-ssh] | ❌ false|unable to read private key: %v", err)
 		return "null", false
 	}
 	// Create the Signer for this private key.
 	signer, err := ssh.ParsePrivateKey(key)
 	//logrus.Info(key)
 	if err != nil {
-		logrus.Error("[util - remote-ssh] | ❌ false|unable to parse private key: %v", err)
+		logrus.Errorf("[util - remote-ssh] | ❌ false|unable to parse private key: %v", err)
 	}
 	//hostKeyCallback, err := kh.New("./data/known_hosts")
 	if err != nil {
-		logrus.Error("[util - remote-ssh] | ❌ false|could not create hostkeycallback function: ", err)
+		logrus.Errorf("[util - remote-ssh] | ❌ false|could not create hostkeycallback function: %v ", err)
 		return "null", false
 	}
 
@@ -47,13 +47,13 @@ func RemoteCmd(IP, Port, User, CMD, idRsaPath string) (string, bool) {
 	// Connect to the remote server and perform the SSH handshake.
 	client, err := ssh.Dial("tcp", address+":"+port, config)
 	if err != nil {
-		logrus.Error("[util - remote-ssh] | ❌ false|unable to connect: %v", err)
+		logrus.Errorf("[util - remote-ssh] | ❌ false|unable to connect: %v", err)
 		return "null", false
 	}
 	defer client.Close()
 	ss, err := client.NewSession()
 	if err != nil {
-		logrus.Error("[util - remote-ssh] | ❌ false|unable to create SSH session: ", err)
+		logrus.Errorf("[util - remote-ssh] | ❌ false|unable to create SSH session: %v", err)
 		return "null", false
 	}
 	defer ss.Close()
