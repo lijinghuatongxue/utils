5803d84ec53905e70a57157305812ce20f2e86d6
lijinghuatongxue <woshilijinghua@gmail.com>
2021-06-22 11:07:02 +0800

    fea: add List FileDir  fea

---
+++ fileOperate/listFileDir.go
@@ -0,0 +1,26 @@
+package fileOperate
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func ListDir(dirPth string, suffix string) (files []string, err error) {
+	files = make([]string, 0, 10)
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil, err
+	}
+	PthSep := string(os.PathSeparator)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+	for _, fi := range dir {
+		if fi.IsDir() { // 忽略目录
+			continue
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //匹配文件
+			files = append(files, dirPth+PthSep+fi.Name())
+		}
+	}
+	return files, nil
+}
--- fileOperateMain.go
+++ fileOperateMain.go
@@ -22,3 +22,14 @@ func ReadFile2StrMain(FileName string, DetailedOutput bool) (string, error) {
 	}
 	return StrOutput, isOk
 }
+func ListFileDir(dirPath, suffix string) (files []string, err error) {
+	ListRes, err := fileOperate.ListDir(dirPath, suffix)
+	if err != nil {
+		logrus.Errorf("util | ListFileDir operate err! \n%s", err)
+	}
+	return ListRes, err
+	//for _, value := range ListRes{
+	//	//fmt.Println(index, "\t",value)
+	//	fmt.Println(value)
+	//}
+}
