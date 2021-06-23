b6e23f5fd6bca740b29bfb26976c83845363f3c3
lijinghuatongxue <woshilijinghua@gmail.com>
2021-06-23 19:53:58 +0800

    test: add  remote_ssh_test.go

---
+++ test/remote_ssh_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"github.com/lijinghuatongxue/utils"
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestRemoteSSH(t *testing.T) {
+	cmd := "docker ps"
+	ip := "192.168.0.123"
+	port := "22"
+	user := "root"
+	RemoteCmdOutput, err := utils.RemoteCmd(ip, port, user, cmd, "")
+	if err != true {
+		logrus.Error("[util - remote-ssh] | ❌ false| Err ===》%s", err)
+		return
+	}
+	logrus.Infof("cmd -> %s |Output -> \n%s", cmd, RemoteCmdOutput)
+}
