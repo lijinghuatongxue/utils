ed220e419aa3f9d525652790ea252e88aa8919cb
woshilijinghua <woshilijinghua@gmail.com>
2022-05-18 17:49:25 +0800

    fea: add sftp upload and download

--- go.mod
+++ go.mod
@@ -8,9 +8,10 @@ require (
 	github.com/go-git/go-git/v5 v5.3.0
 	github.com/google/gopacket v1.1.19
 	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067
+	github.com/pkg/sftp v1.13.4 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
---
+++ sftp_upload_download.go
@@ -0,0 +1,168 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/pkg/sftp"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"time"
+)
+
+func connect(user, password, PublicKeysPath, host string, port int) (*sftp.Client, error) {
+	var (
+		auth         []ssh.AuthMethod
+		addr         string
+		clientConfig *ssh.ClientConfig
+		sshClient    *ssh.Client
+		sftpClient   *sftp.Client
+		err          error
+	)
+	// get auth method
+	auth = make([]ssh.AuthMethod, 0)
+	pemBytes, err := ioutil.ReadFile(PublicKeysPath)
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+	signer, err := ssh.ParsePrivateKey(pemBytes)
+	if err != nil {
+		logrus.Error("parse key failed:%v", err)
+		return nil, err
+	}
+	auth = append(auth, ssh.Password(password), ssh.PublicKeys(signer))
+
+	clientConfig = &ssh.ClientConfig{
+		User:            user,
+		Auth:            auth,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         30 * time.Second,
+	}
+
+	addr = fmt.Sprintf("%s:%d", host, port)
+
+	if sshClient, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
+		return nil, err
+	}
+
+	// create sftp client
+	if sftpClient, err = sftp.NewClient(sshClient); err != nil {
+		return nil, err
+	}
+
+	return sftpClient, nil
+}
+
+func SftpUpload(isPasswd bool, username, passwd, sftpServer, PublicKeysPath, LocalFilePath, RemoteDirPath string, sftpPort int) error {
+	var (
+		err        error
+		sftpClient *sftp.Client
+	)
+	if isPasswd == false {
+		passwd = "xx"
+	}
+	// 这里换成实际的 SSH 连接的 用户名，密码，主机名或IP，SSH端口
+	sftpClient, err = connect(username, passwd, PublicKeysPath, sftpServer, sftpPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(sftpClient *sftp.Client) {
+		err := sftpClient.Close()
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+	}(sftpClient)
+	srcFile, err := os.Open(LocalFilePath)
+	if err != nil {
+		logrus.Error(err)
+		return err
+	}
+	defer func(srcFile *os.File) {
+		err := srcFile.Close()
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+	}(srcFile)
+
+	var remoteFileName = path.Base(LocalFilePath)
+	dstFile, err := sftpClient.Create(path.Join(RemoteDirPath, remoteFileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(dstFile *sftp.File) {
+		err := dstFile.Close()
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+	}(dstFile)
+
+	buf := make([]byte, 1024)
+	for {
+		n, _ := srcFile.Read(buf)
+		if n == 0 {
+			break
+		}
+		dstFile.Write(buf)
+	}
+
+	logrus.Infof("上传成功 ✅ ｜远程文件位置 -> %s", RemoteDirPath+remoteFileName)
+	return nil
+}
+
+func SftpDownload(isPasswd bool, username, passwd, sftpServer, PublicKeysPath, LocalDirPath, RemoteFilePath string, sftpPort int) {
+	var (
+		err        error
+		sftpClient *sftp.Client
+	)
+	if isPasswd == false {
+		passwd = "xx"
+	}
+	// 这里换成实际的 SSH 连接的 用户名，密码，主机名或IP，SSH端口
+	sftpClient, err = connect(username, passwd, PublicKeysPath, sftpServer, sftpPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(sftpClient *sftp.Client) {
+		err := sftpClient.Close()
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+	}(sftpClient)
+
+	srcFile, err := sftpClient.Open(RemoteFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(srcFile *sftp.File) {
+		err := srcFile.Close()
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+	}(srcFile)
+
+	var localFileName = path.Base(RemoteFilePath)
+	dstFile, err := os.Create(path.Join(LocalDirPath, localFileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(dstFile *os.File) {
+		err := dstFile.Close()
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+	}(dstFile)
+
+	if _, err = srcFile.WriteTo(dstFile); err != nil {
+		log.Fatal(err)
+	}
+	logrus.Infof("下载成功 ✅ ｜文件位置 -> %s", LocalDirPath+path.Base(RemoteFilePath))
+}
